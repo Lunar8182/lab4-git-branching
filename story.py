578b18fe24261827ca2854160172ca746958ff0d
Chase Kowalsky <231607559+Lunar8182@users.noreply.github.com>
2025-09-17 17:43:26 -0400

    Begin the hero path with the evil witch encounter

--- story.py
+++ story.py
@@ -11,7 +11,8 @@ def intro():
         print("You stand still, unsure what to do. The forest swallows you.")
 
 def left_path():
-    print("You walk left and find a mysterious glowing sword stuck in a stone.")
+    print("You walk left and find an evil witch.")
+    print("You defeat the evil witch and save the day!")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
